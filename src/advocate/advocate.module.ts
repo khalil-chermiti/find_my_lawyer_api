8a45a9ce049dbdfa1af41728a3e43fec7e8eb438
khalil-chermiti <khalil666chermiti@gmail.com>
2023-12-05 10:34:29 +0100

    feat(lawyer): lawyer can update his profile

--- src/advocate/advocate.module.ts
+++ src/advocate/advocate.module.ts
@@ -5,9 +5,6 @@ import { UploadModule } from '../common/upload/upload.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Avocat, AvocatSchema } from './advocate.schema';
 
-export const { ConfigurableModuleClass } =
-  new ConfigurableModuleBuilder().build();
-
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Avocat.name, schema: AvocatSchema }]),
@@ -16,4 +13,4 @@ export const { ConfigurableModuleClass } =
   controllers: [AdvocateController],
   providers: [AdvocateService],
 })
-export class AdvocateModule extends ConfigurableModuleClass {}
+export class AdvocateModule {}
---
+++ src/advocate/dto/UpdateProfileDTO.ts
@@ -0,0 +1,27 @@
+export class UpdateProfileDTO {
+  prenom: string;
+
+  nom: string;
+
+  email: string;
+
+  bio: string;
+
+  telephone: string;
+
+  ansExperience: number;
+
+  aideJuridique: boolean;
+
+  specialite: string[];
+
+  diplome: string;
+
+  langues: string[];
+
+  honoraires: string[];
+
+  moyenDePaiement: string[];
+
+  adresse: {};
+}
